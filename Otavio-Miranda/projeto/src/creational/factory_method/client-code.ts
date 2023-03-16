7f378db8cdf9c76f5898601b2da5621b860a0603
Lucas Rodrigues <lucas.rodrigues@iter.com.br>
2023-03-16 17:57:46 -0300

    Factory Method parte 2

--- Otavio-Miranda/projeto/src/creational/factory_method/client-code.ts
+++ Otavio-Miranda/projeto/src/creational/factory_method/client-code.ts
@@ -1,10 +1,28 @@
+import { CarFactory } from './factories/car-factory';
+import { randomCarAlgorithm } from './main/random-vehicle-algorithm';
+import { randomNumbers } from './utils/random.numbers';
 import { Car } from './vehicles/car'
 import { Motocycle } from './vehicles/motocycle';
 
 /*const fusca = new Car('Fusca');
 fusca.pickUp('Joana');
-fusca.stop();*/
+fusca.stop();
 
 const motinha = new Motocycle('Motinha')
 motinha.pickUp('Joao')
 motinha.stop()
+
+const carFactory = new CarFactory()
+const fusca = carFactory.getVehicle('fusca')
+fusca.pickUp('joana')
+fusca.stop();*/
+
+const customerNames = ['Ana', "Joana", 'Helena', 'João']
+
+for (let i = 0; i < 10; i++) {
+    const vehicle = randomCarAlgorithm();
+    const name = customerNames[randomNumbers(customerNames.length)]
+    vehicle.pickUp(name);
+    vehicle.stop();
+    console.log('---')
+}
